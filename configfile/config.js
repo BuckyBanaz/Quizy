40f0a959b3347c291b0a09e862fd6b2477b62c60
Sahil Rohilla <rohillasahil704@gmail.com>
2026-02-13 11:18:09 +0530

    new

--- configfile/config.js
+++ configfile/config.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
-
-mongoose.connect("mongodb+srv://sahil:shyambaba@quizyapp.maghgu0.mongodb.net/quizyapp")
+mongoose.connect("mongodb+srv://goquizzytechnology_db_user:sunil%401007@goquizzy.udda5h7.mongodb.net/goquizzy?retryWrites=true&w=majority")
+.then(() => console.log("MongoDB Connected"))
+.catch((err) => console.log(err));
 
 PORT=5000
